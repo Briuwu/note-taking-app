5db5f5905686569b87ed980135ebdb44c3a6427e
Briuwu <millontebrian@gmail.com>
2024-12-20 22:31:43 +0800

    Add database configuration and schema for notes application; include initial data setup

---
+++ db/schema.ts
@@ -0,0 +1,61 @@
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull().unique(),
+  username: text("username").notNull().unique(),
+  email: text("email").notNull().unique(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  notes: many(notes),
+}));
+
+export const tags = pgTable("tags", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull().unique(),
+  noteId: integer("note_id")
+    .notNull()
+    .references(() => notes.id, { onDelete: "cascade" }),
+});
+
+export const tagsRelations = relations(tags, ({ one }) => ({
+  note: one(notes, {
+    fields: [tags.noteId],
+    references: [notes.id],
+  }),
+}));
+
+export const notes = pgTable("notes", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id")
+    .references(() => users.userId, {
+      onDelete: "cascade",
+      onUpdate: "cascade",
+    })
+    .notNull(),
+  title: text("title").notNull(),
+  content: text("content").notNull(),
+  isArchived: boolean("is_archived").notNull().default(false),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdate(() => new Date()),
+});
+
+export const notesRelations = relations(notes, ({ one, many }) => ({
+  author: one(users, {
+    fields: [notes.userId],
+    references: [users.id],
+  }),
+  tags: many(tags),
+}));
